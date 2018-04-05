c818d03ce4a827e0703b40efbaed28cf4804567c
Yac_pardh <pardha.billa@yahoo.com>
2018-04-05 16:16:43 +0530

    template with setup

---
+++ template.rb
@@ -0,0 +1,131 @@
+require "fileutils"
+require "shellwords"
+
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))]
+end
+
+def add_gems
+  gem 'data-confirm-modal', '~> 1.6.2'
+  gem 'devise', '~> 4.4.3'
+  gem 'devise-bootstrapped', github: 'excid3/devise-bootstrapped', branch: 'bootstrap4'
+  gem 'devise_masquerade', '~> 0.6.0'
+  gem 'font-awesome-sass', '~> 4.7'
+  gem 'gravatar_image_tag', github: 'mdeering/gravatar_image_tag'
+  gem 'jquery-rails', '~> 4.3.1'
+  gem 'bootstrap', '~> 4.0.0.beta'
+  gem 'webpacker', '~> 3.0'
+  gem 'foreman', '~> 0.84.0'
+end
+
+def set_application_name
+  # Ask user for application name
+  application_name = ask("What is the name of your application? Default: QuickStart")
+
+  # Checks if application name is empty and add default Jumpstart.
+  application_name = application_name.present? ? application_name : "QuickStart"
+
+  # Add Application Name to Config
+  environment "config.application_name = '#{application_name}'"
+
+  # Announce the user where he can change the application name in the future.
+  puts "Your application name is #{application_name}. You can change this later on: ./config/application.rb"
+end
+
+def add_users
+  # Install Devise
+  generate "devise:install"
+
+  # Configure Devise
+  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
+              env: 'development'
+  route "root to: 'home#index'"
+
+  # Devise notices are installed via Bootstrap
+  generate "devise:views:bootstrapped"
+
+  # Create Devise User
+  generate :devise, "User",
+           "name",
+           "admin:boolean"
+
+  # Set admin default to false
+  in_root do
+    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ":admin, default: false"
+  end
+
+  requirement = Gem::Requirement.new("> 5.2")
+  rails_version = Gem::Version.new(Rails::VERSION::STRING)
+
+  if requirement.satisfied_by? rails_version
+    gsub_file "config/initializers/devise.rb",
+      /  # config.secret_key = .+/,
+      "  config.secret_key = Rails.application.credentials.secret_key_base"
+  end
+
+  # Add Devise masqueradable to users
+  inject_into_file("app/models/user.rb", "masqueradable, :", after: "devise :")
+end
+
+def add_bootstrap
+  # Remove Application CSS
+  run "rm app/assets/stylesheets/application.css"
+
+  # Add Bootstrap JS
+  insert_into_file(
+    "app/assets/javascripts/application.js",
+    "\n//= require jquery\n//= require popper\n//= require bootstrap\n//= require data-confirm-modal",
+    after: "//= require rails-ujs"
+  )
+end
+
+def copy_templates
+  directory "app", force: true
+  directory "config", force: true
+  directory "lib", force: true
+
+  route "get '/terms', to: 'home#terms'"
+  route "get '/privacy', to: 'home#privacy'"
+end
+
+def add_webpack
+  puts "started installing web packer"
+  rails_command "webpacker:install"
+  puts "eding installing web packer"
+end
+
+def add_foreman
+  copy_file "Procfile"
+end
+
+def stop_spring
+  run "spring stop"
+end
+
+
+
+# Main setup
+add_gems
+
+after_bundle do
+  set_application_name
+  stop_spring
+  add_users
+  add_bootstrap
+  add_foreman
+  add_webpack
+
+  copy_templates
+
+  # Migrate
+  rails_command "db:create"
+  rails_command "db:migrate"
+
+  # Migrations must be done before this
+
+
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
